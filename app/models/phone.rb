26356e4c3e96272da3555d677d4d93db6b0ca2bb
nosepass <nosepass8@gmail.com>
2014-09-05 10:10:16 -0700

    documenting some shizz

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,2 +1,7 @@
+# I keep track of phones used to log in for blacklisty purposes
+# The phone identifier string and model information is stored here
+# The identifier is typically whatever is returned by the Android
+# TelephonyManager#getDeviceId(), which some hardware dependent longass string.
+# A hash of detailed build information is also kept alongside the model number.
 class Phone < ActiveRecord::Base
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
+# A user. This is usually autocreated, with a generated password
+# being issued. All other information is optional, if user a cares
+# to create a nickname etc
 class User < ActiveRecord::Base
-  validates :epassword, presence: true,
+  validates :epassword, :salt, presence: true
 end
