718c3d0d66d7164b7d62f97f997ca585a8997a21
nosepass <nosepass8@gmail.com>
2014-11-04 21:05:44 -0800

    still messing with deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,5 @@
 APP_ROOT = File.expand_path(File.dirname(File.dirname(__FILE__)))
  
-if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
-begin
-rvm_path = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
-rvm_lib_path = File.join(rvm_path, 'lib')
-#$LOAD_PATH.unshift rvm_lib_path
-require 'rvm'
-RVM.use_from_path! APP_ROOT
-rescue LoadError
-raise "RVM ruby lib is currently unavailable."
-end
-end
- 
-ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
-require 'bundler/setup'
- 
 worker_processes 4
 working_directory APP_ROOT
  
@@ -22,26 +7,26 @@ preload_app true
  
 timeout 30
  
-listen APP_ROOT + "/tmp/sockets/unicorn.sock", :backlog => 64
+listen APP_ROOT + "/unicorn.sock", :backlog => 64
  
-pid APP_ROOT + "/tmp/pids/unicorn.pid"
+pid APP_ROOT + "/unicorn.pid"
  
 stderr_path APP_ROOT + "/log/unicorn.stderr.log"
 stdout_path APP_ROOT + "/log/unicorn.stdout.log"
  
 before_fork do |server, worker|
-defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
  
-old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
-if File.exists?(old_pid) && server.pid != old_pid
-begin
-Process.kill("QUIT", File.read(old_pid).to_i)
-rescue Errno::ENOENT, Errno::ESRCH
-puts "Old master alerady dead"
-end
-end
+  old_pid = APP_ROOT + '/unicorn.pid.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+  begin
+    Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      puts "Old master alerady dead"
+    end
+  end
 end
  
 after_fork do |server, worker|
-defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
